4bf738df04ecdf4e98d2e145b5f4c7ca023aa902
devnrj07 <niraj.mukta123@gmail.com>
2020-05-06 17:20:53 +0530

    css fixes

--- each day build day!/Day 29/main.js
+++ each day build day!/Day 29/main.js
@@ -13,7 +13,11 @@ function getTheArticle(){
     isLoding = true;
     fetch(searchArticleUrl)
     .then(res=>res.json())
-    .then(data=>console.log(data))
+    .then(data=>{
+        console.log(data)
+        const articles = data.query.search;
+        displayArticles(articles);
+    })
     .then(err=>console.log(err))
     isLoding = false;
 //reset the search bar
@@ -21,7 +25,27 @@ function getTheArticle(){
 
 function displayArticles(articles){
 
+    // Store a reference to `.searchResults`
+  const searchResults = document.querySelector('.searchResults');
+  // Remove all child elements
+  searchResults.innerHTML = '';
+
+  // Loop over results array
+  articles.forEach(article => {
+   const url = encodeURI(`https://en.wikipedia.org/wiki/${article.title}`);
+
+   searchResults.insertAdjacentHTML('beforeend',
+      `<div class="resultItem">
+        <h3 class="resultItem-title">
+          <a href="${url}" target="_blank" rel="noopener">${article.title}</a>
+        </h3>
+        <span class="resultItem-snippet">${article.snippet}</span><br>
+        <a href="${url}" class="resultItem-link" target="_blank" rel="noopener">${url}</a>
+      </div>`
+    );
+  });
 }
+
 
 function displayLoadingSpinner(){
 
